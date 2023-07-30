661826ee9beafaea8a17f3f302ac0676417a64b8
Matheus Fonteles <92957086+matheusmfl@users.noreply.github.com>
2023-07-30 18:21:44 -0300

    Ending Section Product

--- src/app/components/SectionProdutos.tsx
+++ src/app/components/SectionProdutos.tsx
@@ -21,6 +21,20 @@ export function SectionProdutos() {
         <ProductCard />
       </div>
 
+      {/* Textos */}
+      <div>
+        <h3 className="font-roboto text-xl font-normal leading-[32px] text-stone-600 pr-[2px]">
+          Gostou dos nossos produtos? clique no botão abaixo para ver o nosso catálogo completo ou solicitar peças sob medida.
+        </h3>
+      </div>
+
+      {/* botão */}
+      <div>
+        <button className="rounded-[4px] w-full bg-[#8D684A] py-4 shadow-lg text-center text-white font-roboto text-xl font-medium uppercase">
+          faça um orçamento
+        </button>
+      </div>
+
     </section>
   )
 }
