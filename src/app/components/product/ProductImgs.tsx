a5920c4593da66dea16929e5efbeae87655186da
Matheus Fonteles <92957086+matheusmfl@users.noreply.github.com>
2023-08-04 14:09:42 -0300

    Testing image products

--- src/app/components/product/ProductImgs.tsx
+++ src/app/components/product/ProductImgs.tsx
@@ -1,4 +1,6 @@
+'use client'
 import Image from "next/image";
+import { useState } from "react";
 
 
 interface Images {
@@ -11,15 +13,22 @@ interface Product {
 }
 
 export function ProductImgs({ mainImage, thumbs }: Product) {
+  const [currentImage, setCurrentImage] = useState(mainImage);
+
+  const handleThumbnailClick = (imageUrl: string) => {
+    setCurrentImage(imageUrl);
+  };
   return (
-    <main className="flex flex-col gap-[10px] px-3">
+    <main className="flex flex-col gap-[20px] px-3">
       <div className="rounded w-full">
-        <Image src={`${mainImage}`} alt="Alternative" width={400} height={300} className="rounded" />
+        <Image src={`${currentImage}`} alt="Alternative" width={400} height={280} className="rounded aspect-square h-[280px]" />
       </div>
       <div className="flex gap-2 overflow-x-auto pb-2">
         {thumbs &&
           thumbs.map((image, index) => (
-            <div key={index} className="w-[91px] h-[82px] aspect-video cursor-pointer relative">
+            <div key={index}
+              className="w-[91px] h-[82px] aspect-video cursor-pointer relative "
+              onClick={() => handleThumbnailClick(image.url)}>
               <Image src={image.url} alt={image.alt} fill className="rounded-lg absolute" />
             </div>
           ))}
